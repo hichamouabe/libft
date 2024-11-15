d848eadc25bbf185fdd58181be3721352eb20953
hicham ouabella <121818057+hichamouabe@users.noreply.github.com>
2024-11-15 19:58:10 +0100

    Update ft_putendl_fd.c

--- libft_github/ft_putendl_fd.c
+++ libft_github/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: houabell <houabell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 20:18:13 by houabell          #+#    #+#             */
-/*   Updated: 2024/11/10 19:43:58 by houabell         ###   ########.fr       */
+/*   Updated: 2024/11/14 21:29:46 by houabell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if (fd < 0)
+	if (fd < 0 || !s)
 		return ;
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
